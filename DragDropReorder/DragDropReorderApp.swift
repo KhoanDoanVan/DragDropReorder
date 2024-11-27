f201a1836e60b42a4047f72bd6fdad2c80d63593
khoanisdev <doanvankhoan124@gmail.com>
2024-11-27 11:13:58 +0700

    Initial Commit

---
+++ DragDropReorder/DragDropReorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropReorderApp.swift
+//  DragDropReorder
+//
+//  Created by Đoàn Văn Khoan on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DragDropReorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
